056246531aa911a6c51a6db3ba44239b171cb6d9
Erick <vargas.erick030997@gmail.com>
2019-05-14 11:50:55 -0500

    Update

---
+++ ArtistApp/app/src/main/java/com/crypto/artist/digitalportrait/Orders/Principal/OrdersMain.java
@@ -0,0 +1,16 @@
+package com.crypto.artist.digitalportrait.Orders.Principal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.crypto.artist.digitalportrait.R;
+
+public class OrdersMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_orders_main);
+    }
+}
