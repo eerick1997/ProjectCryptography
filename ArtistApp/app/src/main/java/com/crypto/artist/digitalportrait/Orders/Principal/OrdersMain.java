af8b865b75092bb1098f706b7a7db8e6e6c22ca9
Alberto Esquivel <albertoesquivel.97@gmail.com>
2019-05-18 17:10:12 -0500

    Obtener pedidos
    Obtener pedidos para el artista actualizado

--- ArtistApp/app/src/main/java/com/crypto/artist/digitalportrait/Orders/Principal/OrdersMain.java
+++ ArtistApp/app/src/main/java/com/crypto/artist/digitalportrait/Orders/Principal/OrdersMain.java
@@ -2,23 +2,32 @@ package com.crypto.artist.digitalportrait.Orders.Principal;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
+
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.crypto.artist.digitalportrait.Orders.Adapter.OrdersAdapter;
+import com.crypto.artist.digitalportrait.Orders.Objects.Datos;
 import com.crypto.artist.digitalportrait.Orders.Objects.Order;
 import com.crypto.artist.digitalportrait.R;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Executor;
 
 public class OrdersMain extends BottomSheetDialogFragment {
 
@@ -30,7 +39,7 @@ public class OrdersMain extends BottomSheetDialogFragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View itemView = inflater.inflate(R.layout.activity_orders_main, container, false);
 
-        List<Order> orders = new ArrayList<>();
+        final List<Order> orders = new ArrayList<>();
         orders.add( new Order("Accepted", "15\\01\\2019") );
         orders.add( new Order("Waiting", "15\\02\\2019") );
         orders.add( new Order("Accepted", "15\\03\\2019") );
@@ -42,12 +51,36 @@ public class OrdersMain extends BottomSheetDialogFragment {
         orders.add( new Order("Accepted", "15\\09\\2019") );
         orders.add( new Order("Waiting", "15\\10\\2019") );
 
-        RecyclerView recyclerOrders = itemView.findViewById(R.id.recycler_orders);
-        OrdersAdapter ordersAdapter = new OrdersAdapter(getContext(), orders);
+        final RecyclerView recyclerOrders = itemView.findViewById(R.id.recycler_orders);
 
-        recyclerOrders.setHasFixedSize(true);
-        recyclerOrders.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
-        recyclerOrders.setAdapter(ordersAdapter);
+
+
+        FirebaseFirestore db= FirebaseFirestore.getInstance();
+        CollectionReference datosReference=db.collection("usuarios").document("clientes").collection("cliente1");
+        datosReference.addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
+                orders.clear();
+                if(e!=null){
+                    return;
+                }
+                for(QueryDocumentSnapshot documentSnapshot: queryDocumentSnapshots) {
+                    Datos datos = documentSnapshot.toObject(Datos.class);
+                    datos.setDocumentId(documentSnapshot.getId());
+
+                    orders.add(new Order(datos.getDescripcion(),datos.getFecha()));
+                }
+                Log.d(TAG, "onEvent: pedidos = " + orders);
+                //Creamos el adaptador
+                OrdersAdapter ordersAdapter = new OrdersAdapter(getContext(), orders);
+                //Colocamos el adaptador
+                recyclerOrders.setHasFixedSize(true);
+                recyclerOrders.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
+                recyclerOrders.setAdapter(ordersAdapter);
+            }
+        });
+
+
 
         return  itemView;
     }
